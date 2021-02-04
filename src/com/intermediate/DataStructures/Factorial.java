9a4db14fe61d1222a9aa75e2526930f42780cc36
rmssriram <35577885+rmssriram@users.noreply.github.com>
2021-02-03 22:36:21 -0500

    Add files via upload

---
+++ src/com/intermediate/DataStructures/Factorial.java
@@ -0,0 +1,14 @@
+package com.intermediate.DataStructures;
+
+// Find Factorial
+public class Factorial {
+    public int fact(int n){
+        if(n==0){
+            return 1;
+        }
+        else
+        {
+            return(n*fact(n-1));
+        }
+    }
+}
